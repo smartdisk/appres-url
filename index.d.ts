51797d165f7e59ff7b946b5a5a201ccac333a6ca
SMARTDISK <smartdisk@icloud.com>
2021-01-09 08:23:02 +0900

    encodeComp

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,8 @@
 export declare class UrlEx {
   static encode(url: string): string;
   encode(url: string): string;  
+  static encodeComp(url: string): string;
+  encodeComp(url: string): string;  
   static decode(encoded: string): string;
   decode(encoded: string): string;  
 }
--- index.js
+++ index.js
@@ -13,8 +13,7 @@ var UrlEx = /** @class */ (function () {
   function _encode (url = '') {
     if (!_isString(url)) {
       throw new Error('Invalid');
-    }
-    
+    }    
     return encodeURIComponent(url)
       .replace(/!/g, '%21')
       .replace(/'/g, '%27')
@@ -25,7 +24,19 @@ var UrlEx = /** @class */ (function () {
       .replace(/%2F/g, '/')
       .replace(/%20/g, '+');
   }
-      
+  function _encodeComp (url = '') {
+    if (!_isString(url)) {
+      throw new Error('Invalid');
+    }    
+    return encodeURIComponent(url)
+      .replace(/!/g, '%21')
+      .replace(/'/g, '%27')
+      .replace(/\(/g, '%28')
+      .replace(/\)/g, '%29')
+      .replace(/\*/g, '%2A')
+      .replace(/%20/g, '+');
+  }
+
   function _decode (encoded = '') {
     if (!_isString(encoded)) {
       throw new Error('Invalid');
@@ -37,6 +48,9 @@ var UrlEx = /** @class */ (function () {
   UrlEx.prototype.encode = function(url){
     return _encode(url);
   };
+  UrlEx.prototype.encodeComp = function(url){
+    return _encodeComp(url);
+  };
   UrlEx.prototype.decode = function(encoded){
     return _decode(encoded);
   };
@@ -44,6 +58,9 @@ var UrlEx = /** @class */ (function () {
   UrlEx.urlex = new UrlEx();
   UrlEx.encode = function(url) {
     return this.urlex.encode(url);
+  };
+  UrlEx.encodeComp = function(url) {
+    return this.urlex.encodeComp(url);
   };
   UrlEx.decode = function(encoded) {
     return this.urlex.decode(encoded);
@@ -57,11 +74,13 @@ module.exports.UrlEx = UrlEx;
 
 /*
 // Samples
-let encoded = UrlEx.encode("https://abc.com/123?a=value&b=value");
+let encoded1 = UrlEx.encode("https://abc.com/123?a=value&b=value");
+let encoded2 = UrlEx.encodeComp("https://abc.com/123?a=value&b=value");
 let decoded1 = UrlEx.decode("https://abc.com/123%3Fa%3Dvalue%26b%3Dvalue");
 let decoded2 = UrlEx.decode("https%3A%2F%2Fabc.com%2F123%3Fa%3Dvalue%26b%3Dvalue");
 
-console.log("encoded=" + encoded);
+console.log("encoded1=" + encoded1);
+console.log("encoded2=" + encoded2);
 console.log("decoded1=" + decoded1);
 console.log("decoded2=" + decoded2);
 */
